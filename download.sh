6200405059cd938f38e4e910376592c32c36209a
littlekobe <aifumu.wx@gmail.com>
2018-01-19 10:18:31 -0800

    brute force download

---
+++ download.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSeEpDajIwOUFhaGc?alt=media -o train_split.0.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSZnZPY1dmaHJzMHc?alt=media -o train_split.1.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSb0VjVDJ3am40VVE?alt=media -o train_split.2.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSTmQtd1VfWWFyUHM?alt=media -o train_split.3.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSQ1ozYmlITXlUaDQ?alt=media -o train_split.4.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSTVY1MnFGV0JiVkk?alt=media -o train_split.5.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSYmhmbnp6d2I4a2M?alt=media -o train_split.6.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSZl9aNGVuX0llcEU?alt=media -o train_split.7.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSWXJ3R3hsZllsNVk?alt=media -o train_split.8.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSR2N4cFpweURhTjg?alt=media -o train_split.9.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViScllKWnlaVU53Skk?alt=media -o train_split.10.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSV2QxZW1rVXcxT1U?alt=media -o train_split.11.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSNGNPTEFhdGxkMnM?alt=media -o train_split.12.tar.gz
+
+curl -H "Authorization: Bearer ya29.GltHBWy0p4DACNSDtuIWBmk-9eksForWxFnbVg-IzZzaph_x5vC0Ux40-4x0JF_RochB9xcYBxTvPn9ukIwUNRER0ZTEvepjtj7TcL6Mkz40bipOU86Xf4QpmK2i" https://www.googleapis.com/drive/v3/files/0ByQS_kT8kViSWmtRa1lMcG1EaHc?alt=media -o val_images.tar.gz
+
